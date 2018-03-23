06d7714ca7ab35cde7cead2fbe69f09f4d37a38e
[(Dat[X_O]wl)] <37256245+SamuelNZ@users.noreply.github.com>
2018-03-24 04:36:20 +1300

    delete batch
    delete batch

--- OFF.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-for /f "tokens=14" %%a in ('ipconfig ^| findstr IPv4') do set _IPaddr=%%a
-echo YOUR IP ADDRESS IS: %_IPaddr%
-java -jar target\VMRadar-1.2.1-jar-with-dependencies.jar %_IPaddr% PortFilter %_IPaddr% Offline
- 
-timeout 1000
